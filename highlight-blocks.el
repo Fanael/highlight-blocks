4288d860a4210f848f02270a51a14fc016eb06f9
Fanael Linithien <fanael4@gmail.com>
2014-10-18 23:14:57 +0200

    Release version 0.1.10.

--- highlight-blocks.el
+++ highlight-blocks.el
@@ -2,7 +2,7 @@
 
 ;; Author: Fanael Linithien <fanael4@gmail.com>
 ;; URL: https://github.com/Fanael/highlight-blocks
-;; Version: 0.1.9
+;; Version: 0.1.10
 ;; Package-Requires: ((emacs "24"))
 
 ;; This file is NOT part of GNU Emacs.
